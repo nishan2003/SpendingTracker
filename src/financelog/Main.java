719708b565ca6e492e6551d2e975c20fd792917d
nishan2003 <43458723+nishan2003@users.noreply.github.com>
2020-10-05 15:04:50 -0600

    Changes to code

--- src/financelog/Main.java
+++ src/financelog/Main.java
@@ -152,29 +152,7 @@ public class Main extends Application {
         InputPasswordLogin.setPromptText("Password");
         Login = new Button("Login");
         Login.setOnAction(e -> {
-            String[] Array = new String[15];
-            String line = null;
-            try {
-                Scanner fileReader = new Scanner(new File(InputUserLogin.getText()));
-                while (fileReader.hasNext()) {
-                    line = fileReader.nextLine();
-                }
-                Array = line.split(" ---- ");
-
-            } catch (FileNotFoundException fileNotFoundException) {
-                fileNotFoundException.printStackTrace();
-            }
-
-
-            if(InputUserLogin.getText().equals(Array[0]) && InputPasswordLogin.getText().equals(Array[1])) {
-                System.out.println("Nice");
-            }
-            else {
-                OkAlert.popUp("Error", "Username or Password is incorrect.", Color.RED);
-            }
-            InputUserLogin.clear();
-            InputPasswordLogin.clear();
-
+            CheckLogin.Login(InputUserLogin, InputPasswordLogin);
         });
         ExitLogin = new Button("Exit");
         ExitLogin.setOnAction(e -> {
